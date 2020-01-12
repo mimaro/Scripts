42f1049d1e245bf52f9875fd86a8856bea5ca4af
mimaro <46071857+mimaro@users.noreply.github.com>
2020-01-12 14:18:43 +0100

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -5,6 +5,6 @@ import time, sys
 sensor1 = '/sys/bus/w1/devices/28-021492459fbf/w1_slave'
 
 def   readTemp(sensor1):
-      t_s_1 = readtemp(sensor1)
-
+      
+t_s_1 = readtemp(sensor1)
 print (t_s_1)
