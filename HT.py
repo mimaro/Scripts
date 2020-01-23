f97168a7a34e232065961344c0c70add32bd277f
mimaro <46071857+mimaro@users.noreply.github.com>
2020-01-23 23:25:39 +0100

    Update HT.py

--- HT.py
+++ HT.py
@@ -13,10 +13,10 @@ VZ_POST_URL = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=ad
 
 #Umschaltzeiten Hoch- Niedertarig
 
-HT_ein_Mo_Fr datetime.time(6, 0)
-HT_aus_Mo_Fr datetime.time(22, 0)
-HT_ein_Sa datetime.time(6, 0)
-HT_aus_Sa datetime.time(13, 0)
+HT_ein_Mo_Fr = datetime.time(6, 0)
+HT_aus_Mo_Fr = datetime.time(22, 0)
+HT_ein_Sa = datetime.time(6, 0)
+HT_aus_Sa = datetime.time(13, 0)
 
 #######################################################################################################
 # Configuration
