784bb7e27d11c0ccb44bf19dc8b508ffc0c49998
mimaro <46071857+mimaro@users.noreply.github.com>
2020-11-20 17:10:53 +0100

    Update COP.py

--- COP.py
+++ COP.py
@@ -14,13 +14,8 @@ VZ_POST_URL = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=ad
 # Configuration
 UUID = {
     "COP_o_venti": "312ec8e0-52e7-11e9-ac6d-4f4dd87fd97b",
-    "COP_m_venti": "3a89b8a0-52e7-11e9-b184-73ec439c39c9",
     "WP_th": "9399ca80-910c-11e9-ac0f-31ff5bbdf885",
     "WP_el": "92096720-35ae-11e9-a74c-534de753ada9",
-    "Venti": "8cbbcb70-3c0d-11e9-87f9-9db68697df1d",
-    "HG_VL": "cd28ac80-3630-11ea-b83a-3dc3acc4c33d",
-    "HG_RL": "d06db0f0-3630-11ea-8cc1-9bd4441efb71",
-    "P_therm": "fcb6eb20-3707-11ea-b5f8-73f2af2f9a9b"
 }
 
 ###########################################################################################################
@@ -42,19 +37,8 @@ def main():
     logging.info("COP")
     wp_therm = get_vals(UUID["WP_th"])["data"]["tuples"][0][1]
     wp_el = get_vals(UUID["WP_el"])["data"]["tuples"][0][1]
-    venti = get_vals(UUID["Venti"])["data"]["tuples"][0][1]
-    t_rl = get_vals(UUID["HG_VL"])["data"]["tuples"][0][1]
-    t_vl = get_vals(UUID["HG_RL"])["data"]["tuples"][0][1]
     cop_o_venti = wp_therm / wp_el
-    cop_m_venti = wp_therm / (wp_el + venti)
-    p_therm_HG = (t_vl-t_rl)*1160*1.3
     write_vals(UUID["COP_o_venti"], cop_o_venti)
-    write_vals(UUID["COP_m_venti"], cop_m_venti)
-    write_vals(UUID["P_therm"], p_therm_HG)
-    print (p_therm_HG)
-    print (t_rl)
-    print (t_vl)
-
- 
+  
 if __name__ == "__main__":
     main()
