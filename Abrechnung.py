0f7b3ab4341a88c412910ef63ae7ad4194efdd7c
mimaro <46071857+mimaro@users.noreply.github.com>
2021-01-08 20:06:23 +0100

    Update Abrechnung.py

--- Abrechnung.py
+++ Abrechnung.py
@@ -113,11 +113,11 @@ def main():
     r_n_8c = r_8b/(r_8a + r_8b + r_8c + 0.00001) * r_zev
        
     #Umschaltzeiten Hoch- Niedertarig
-    tz = pytz.UCT+1
+    tz = pytz.UTC+1
     logging.basicConfig(level=logging.INFO)
     now = datetime.datetime.now(tz=tz)
     today = datetime.date.today()
-    logging.info("UCT time: {}".format(now))
+    logging.info("UTC+1 time: {}".format(now))
     logging.info("*****************************")
          
     time = now.time()
