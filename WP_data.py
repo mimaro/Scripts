d3f87c9b1ec109065e27a3e5e01c091955b9cd66
mimaro <46071857+mimaro@users.noreply.github.com>
2021-04-08 10:59:52 +0200

    Update WP_data.py

--- WP_data.py
+++ WP_data.py
@@ -41,5 +41,5 @@ CLIENT = ModbusTcpClient(IP_ISG)
 #value_1 = CLIENT.read_holding_registers(1501, count=1)
 #print(value_1)
 
-Temp_2 =CLIENT.read_input_registers(502, count=8)
+Temp_2 =CLIENT.read_input_registers(502, count=1)
 print(Temp_2)
