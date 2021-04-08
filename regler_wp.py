af6c849c6497cfc76ff02a8f4462d0ffb3023e7a
mimaro <46071857+mimaro@users.noreply.github.com>
2021-04-08 10:30:03 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -336,7 +336,7 @@ def main():
         
     #REGISTER["Betriebsart"]    
         
-    Betrieb = CLIENT.read_holding_registers(1501, count=1)
+    Betrieb = CLIENT.read_input_registers(501, count=1)
     print(Betrieb)
     #logging.info("Betriebszustand: {}".format(Betrieb)) 
     #write_vals(UUID["Betrieb_Z"], Betrieb)
