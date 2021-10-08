a2b2c4240c746b38db4b49acd992e69084c24e6b
mimaro <46071857+mimaro@users.noreply.github.com>
2021-10-08 11:30:51 +0200

    Create alarm.py

---
+++ alarm.py
@@ -0,0 +1,12 @@
+import requests
+import json
+import pprint
+import datetime
+import logging
+import pytz
+
+#######################################################################################################
+# Format URLs
+VZ_GET_URL = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?from={}"
+
+
