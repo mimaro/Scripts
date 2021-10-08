ac7fd7a0d34bc7d49175f7abab73fe5246b5a180
mimaro <46071857+mimaro@users.noreply.github.com>
2021-10-08 11:48:46 +0200

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -9,4 +9,17 @@ import pytz
 # Format URLs
 VZ_GET_URL = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?from={}"
 
+#######################################################################################################
+# UUID's
 
+UUID = {
+  "T_Puffer": "88b7c280-1cab-11e9-938e-fb5dc04c61d4",
+  "PV_Prod": "101ca060-50a3-11e9-a591-cf9db01e4ddd"
+  
+}
+
+#######################################################################################################
+
+
+WP_check = get_vals(UUID["T_Puffer"], duration="-1min")["data"]["average"]
+PV_check = get_vals(UUID["PV_Prod"], duration="-1min")["data"]["average"]
