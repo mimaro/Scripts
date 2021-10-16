d360188b01e26f53646426f53bd3438f943d7360
mimaro <46071857+mimaro@users.noreply.github.com>
2021-10-16 11:51:30 +0200

    Update myStromSwitch.py

--- myStromSwitch.py
+++ myStromSwitch.py
@@ -2,7 +2,8 @@ import requests
 import json
 import logging
 
-UUID = "8cbbcb70-3c0d-11e9-87f9-9db68697df1d"
+UUID_P = "8cbbcb70-3c0d-11e9-87f9-9db68697df1d"
+UUID_T = "	84d8d720-a657-11e9-8c76-3f13823c5e95"
 IP_VENTI = "192.168.178.30"
 URL_VZ = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=add&value={}"
 
@@ -12,16 +13,20 @@ def main():
     data = req.content
     decoded_data = json.loads(data)
     print("Actual Power {}".format(decoded_data["power"]))
+    print("Actual Temperature {}".format(decoded_data["temperature"]))
     print("Posting to VZ")
-    poststring = URL_VZ.format(UUID, decoded_data["power"])
-    postreq = requests.post(poststring)
-    print(poststring)
-    print(postreq.ok)
-
-
-
+    poststring_p = URL_VZ.format(UUID_P, decoded_data["power"])
+    poststring_t = URL_VZ.format(UUID_T, decoded_data["temperature"])
+    postreq_p = requests.post(poststring_p)
+    postreq_t = requests.post(poststring_t)
+    print(poststring_p)
+    print(poststring_t)
+    print(postreq_p.ok)
+    print(postreq_t.ok)
 
 
 if __name__ == "__main__":
     main()
+
+    
     
