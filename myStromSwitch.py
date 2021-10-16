837bf04757767c422f2679ed104f047de6869639
mimaro <46071857+mimaro@users.noreply.github.com>
2021-10-16 12:03:15 +0200

    Update myStromSwitch.py

--- myStromSwitch.py
+++ myStromSwitch.py
@@ -3,7 +3,6 @@ import json
 import logging
 
 UUID_P = "8cbbcb70-3c0d-11e9-87f9-9db68697df1d"
-UUID_T = "84d8d720-a657-11e9-8c76-3f13823c5e95"
 IP_VENTI = "192.168.178.30"
 URL_VZ = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=add&value={}"
 
@@ -14,18 +13,12 @@ def main():
     data = req.content
     decoded_data = json.loads(data)
     print(decoded_data)
-#     print("Actual Power {}".format(decoded_data["power"]))
-#     print("Actual Temperature {}".format(decoded_data["temperature"]))
-#     print("Posting to VZ")
-#     poststring_p = URL_VZ.format(UUID_P, decoded_data["power"])
-#     poststring_t = URL_VZ.format(UUID_T, decoded_data["temperature"])
-#     postreq_p = requests.post(poststring_p)
-#     postreq_t = requests.post(poststring_t)
-#     print(poststring_p)
-#     print(poststring_t)
-#     print(postreq_p.ok)
-#     print(postreq_t.ok)
-
+    print("Actual Power {}".format(decoded_data["power"])
+    print("Posting to VZ")
+    poststring_p = URL_VZ.format(UUID_P, decoded_data["power"])
+    postreq_p = requests.post(poststring_p)
+    print(poststring_p)
+    print(postreq_p.ok)
 
 
 
