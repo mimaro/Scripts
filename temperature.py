3bb05aec07cecab089e5b472f7317b22893627ac
mimaro <46071857+mimaro@users.noreply.github.com>
2021-11-02 16:23:28 +0100

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -67,7 +67,7 @@ def readTempLines(sensorName) :
 
 temp_1 =  str(readTempLines(sensor1)[0])
 temp_2 =  str(readTempLines(sensor2)[0])
-temp_3 =  str(readTempLines(sensor3)[0])
+temp_3 =  str(readTempLines(sensor3)[0]-6)
 temp_4 =  str(readTempLines(sensor4)[0])
 temp_5 =  str(readTempLines(sensor5)[0]+1.4) 
 temp_6 =  str(readTempLines(sensor6)[0]+1.4) 
