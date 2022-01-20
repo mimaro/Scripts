485cdae2e572e30a7870e25f19f20f274965f364
mimaro <46071857+mimaro@users.noreply.github.com>
2022-01-21 00:45:35 +0100

    Update bsf.py

--- bsf.py
+++ bsf.py
@@ -5,7 +5,7 @@ import requests
 import json
 import logging
 
-UUID_P = "ad5c8090-3698-11ea-8ad7-7f796afef9a1"
+#UUID_P = "ad5c8090-3698-11ea-8ad7-7f796afef9a1"
 UUID_T = "70d65570-4a61-11e9-b638-fb0f3e7a4677"
 IP_VENTI = "192.168.178.102"
 URL_VZ = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=add&value={}"
@@ -21,13 +21,13 @@ def main():
     print("Actual Power {}".format(decoded_data["power"]))
     print("Actual Temperature {}".format(decoded_data["temperature"]+ Offset))
     print("Posting to VZ")
-    poststring_p = URL_VZ.format(UUID_P, decoded_data["power"])
+    #poststring_p = URL_VZ.format(UUID_P, decoded_data["power"])
     poststring_t = URL_VZ.format(UUID_T, decoded_data["temperature"]+ Offset)
-    postreq_p = requests.post(poststring_p)
+    #postreq_p = requests.post(poststring_p)
     postreq_t = requests.post(poststring_t)
-    print(poststring_p)
+    #print(poststring_p)
     print(poststring_t)
-    print(postreq_p.ok)
+    #print(postreq_p.ok)
     print(postreq_t.ok)
 
 
