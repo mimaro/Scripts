c41166fb3d0397fda053357ac97ec6a7e1cce299
mimaro <46071857+mimaro@users.noreply.github.com>
2022-03-09 07:44:58 +0100

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -188,7 +188,7 @@ def main():
     
     HK2_min 
     
-    VL_Temp_Soll_min = HK2_Steigung * 1.8317984*(HK2_min-t_now)^0.8281902 + HK2_min
+    VL_Temp_Soll_min = HK2_Steigung * 1.8317984*(HK2_min-t_now)**0.8281902 + HK2_min
     logging.info("SOLL min VL-Temp: {}".format(VL_Temp_Soll_min))
         
     logging.info(f"---------- Prüfung Freigabe / Sperrung Warmwasserbetrieb ----------") 
