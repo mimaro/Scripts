eca2fce759b4241ddd82db5faad77f27a916dfbf
mimaro <46071857+mimaro@users.noreply.github.com>
2022-03-28 13:16:49 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -213,7 +213,7 @@ def main():
     d_time = now_CH - now_UTC
     
     data = json.loads(r.content)
-    sunset = data['results']['sunset'] # Daten für Sonnenuntergang
+    sunset = int(data['results']['sunset']) # Daten für Sonnenuntergang
    
     sunset_time_UTC = datetime.datetime(sunset)
     #sunset_time_UTC = datetime.time(int(sunset[11:13]), int(sunset[14:16])) # Sonnenuntergang in Zeit-Format umwandeln
