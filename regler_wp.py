9a14c26db6921e96645a2d827b065e34f782b0e3
mimaro <46071857+mimaro@users.noreply.github.com>
2022-03-28 12:58:49 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -223,7 +223,7 @@ def main():
     #sunset_time_CH = (sunset + datetime.timedelta(hours=9)).strftime('%H:%M:%S')
 
     logging.info("sunset time: {}".format(sunset_time_UTC))
-    logging.info("sunset time: {}".format(sunset_time_CH))
+    #logging.info("sunset time: {}".format(sunset_time_CH))
     
     logging.info("Swiss time: {}".format(now_CH))
     logging.info("UTC time: {}".format(now_UTC))
