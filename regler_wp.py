dbcda90a84d635b8db57c7414ab670b9e4abddd1
mimaro <46071857+mimaro@users.noreply.github.com>
2022-03-28 11:47:17 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -217,11 +217,13 @@ def main():
     #sunset_time = time(int(sunset[11:13]), int(sunset[14:16])) # Sonnenuntergang in Zeit-Format umwandeln
     
  
-    logging.info("Swiss time: {}".format(now.hour()))
+    logging.info("Swiss time: {}".format(now.time()[0]))
     
     tz_UTC = pytz.utc
     now_UTC = datetime.datetime.now(tz=tz_UTC)
-    logging.info("UTC time: {}".format(now_UTC.hour()))
+    
+    
+    logging.info("UTC time: {}".format(now_UTC.hour))
     
     
     logging.info(f"---------- Prüfung Freigabe / Sperrung Warmwasserbetrieb ----------") 
