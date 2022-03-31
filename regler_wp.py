f91d9b9d20bfda27f77fc8125d6e70744a910c7d
mimaro <46071857+mimaro@users.noreply.github.com>
2022-03-31 11:46:33 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -228,7 +228,7 @@ def main():
     
     t_delta_sunset_freigabe = 9
     
-    t_sunset_freigabe = datetime.strptime((sunset_time_CH - datetime.timedelta(hours=t_delta_sunset_freigabe), '%Y-%m-%d-%H-%M-%S').time()
+    t_sunset_freigabe = datetime.strptime((sunset_time_CH - datetime.timedelta(hours=t_delta_sunset_freigabe)), '%Y-%m-%d-%H-%M-%S').time()
     
     #now_time = datetime.strptime(now, '%Y-%m-%d-%H-%M').time()
     
