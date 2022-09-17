3124f088359e2d631a397adff2b06b7cd18b4247
mimaro <46071857+mimaro@users.noreply.github.com>
2022-09-17 14:48:40 +0200

    Update WP_data.py

--- WP_data.py
+++ WP_data.py
@@ -59,11 +59,11 @@ def write_vals(uuid, val):
     #logging.info("Ok? {}".format(postreq.ok))
 
 #Vorlage read input registers
-AT = CLIENT.read_input_registers("Aussentemp", count=1, unit=1).getRegister(0)
-print(value_1)
+#AT = CLIENT.read_input_registers("Aussentemp", count=1, unit=1).getRegister(0)
+#print(value_1)
 
 #Vorlage read input registers
-value_1 = CLIENT.read_input_registers(506, count=1, unit=1).getRegister(0)
+value_1 = CLIENT.read_input_registers(UUID["Aussentemp"], count=1, unit=1).getRegister(0)
 print(value_1)
 
 #Vorlage read holding registers
