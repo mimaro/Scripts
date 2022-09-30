236275c4af7d81204980378e078d00cc13756248
mimaro <46071857+mimaro@users.noreply.github.com>
2022-09-30 14:21:02 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -72,8 +72,8 @@ T_FREIGABE_MAX = 12
 #Parameter WW-Ladung
 ww_start = datetime.time(12, 0)
 ww_stop = datetime.time(15, 0)
-ww_soll = 46
-ww_aus = 46 #Diese Temperatur muss erreicht werden damit WW-Betrieb beendet wird (VL-Temp WP)
+ww_soll = 45
+ww_aus = 43 #Diese Temperatur muss erreicht werden damit WW-Betrieb beendet wird (VL-Temp WP)
 ww_hyst = 5 #Hysterese für Freigabe WW-Betrieb  
 
 REGISTER = {
