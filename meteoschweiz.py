31d08b025acab936daa8b08f1d4c0d316289a7a0
mimaro <46071857+mimaro@users.noreply.github.com>
2023-10-07 17:37:37 +0200

    Update meteoschweiz.py

--- meteoschweiz.py
+++ meteoschweiz.py
@@ -1,37 +1,26 @@
 import requests
 import csv
 
-UUID_BUS_GS = "bcc12cb0-6290-11e9-acca-29f0ac50e804"
-UUID_MOA_GS = "4f769670-52e4-11e9-840c-29fd59f140bc"
-UUID_SMA_GS = "a4f39770-3698-11ea-b87e-9f684e384f0b"
-
-UUID_BUS_WS = "cf251900-6290-11e9-b8c7-c1df28b9badb"
-UUID_MOA_WS = "ca315260-6290-11e9-86e7-519ba48bed8f"
+UUID_BUS_GS = "159392d0-6521-11ee-9c72-dd2421c1d835"
 
 GS = "gre000z0"
 WS = "fu3010z0"
 
-URL_VZ = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=add&value={}"
+URL_VZ = "192.168.178.49/middleware.php/data/{}.json?operation=add&value={}"
 
 CSV_URL = "https://data.geo.admin.ch/ch.meteoschweiz.messwerte-aktuell/VQHA80.csv"
 
 def main():
-    BUS, MOA, SMA = {}, {}, {}
+    BUS = {}
     req=requests.get(CSV_URL)
     data = req.content.split("\n")[:]
     reader = csv.DictReader(data, delimiter = ';')
     for row in reader:
         if row['Station/Location'] == "BUS":
             BUS=row
-        elif row['Station/Location'] == "MOA":
-            MOA=row
-        elif row['Station/Location'] == "SMA":
-            SMA=row 
+
     requests.post(URL_VZ.format(UUID_BUS_GS, BUS[GS]))
-    requests.post(URL_VZ.format(UUID_MOA_GS, MOA[GS]))
-    requests.post(URL_VZ.format(UUID_SMA_GS, SMA[GS]))
-    requests.post(URL_VZ.format(UUID_BUS_WS, BUS[WS]))
-    requests.post(URL_VZ.format(UUID_MOA_WS, MOA[WS]))
+   
 
 if __name__ == "__main__":
     main()
