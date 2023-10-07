ee519d9b378b1c59169ce1802bc77b4c2636d600
mimaro <46071857+mimaro@users.noreply.github.com>
2023-10-07 17:35:42 +0200

    Update bsf.py

--- bsf.py
+++ bsf.py
@@ -5,10 +5,10 @@ import requests
 import json
 import logging
 
-#UUID_P = "ad5c8090-3698-11ea-8ad7-7f796afef9a1"
-UUID_T = "70d65570-4a61-11e9-b638-fb0f3e7a4677"
+#UUID_P = "ad5c809"
+UUID_T = "78afd3c0-6523-11ee-980e-9fe998eb4bc6"
 IP_VENTI = "192.168.178.37"
-URL_VZ = "http://vz.wiuhelmtell.ch/middleware.php/data/{}.json?operation=add&value={}"
+URL_VZ = "192.168.178.49/middleware.php/data/{}.json?operation=add&value={}"
 
 Offset = -2.1
 
