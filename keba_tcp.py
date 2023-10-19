3b133d6a32c8d95a02ad8e500a4e30bc230f8883
mimaro <46071857+mimaro@users.noreply.github.com>
2023-10-19 09:37:31 +0200

    Update keba_tcp.py

--- keba_tcp.py
+++ keba_tcp.py
@@ -23,8 +23,8 @@ UUID = {
     "P_EIV": "96d53fc0-683f-11ee-bd3d-c5441b8ec095"
 }
 
-modbus_host = "192.168.178.59"
-modbus_port = 502
+server_ip = "192.168.178.59"
+server_port = 502
 unit_id = 1
 charge_state= 1000
 char_curr_1 = 1008
@@ -45,20 +45,57 @@ def write_vals(uuid, val):
     #logging.info("Ok? {}".format(postreq.ok))
    
 def main():  
-    
-    # Create a Modbus TCP client
-    client = ModbusTcpClient(modbus_host, port=modbus_port)
 
-    # Connect to the Modbus device
+    # Define the Modbus TCP client
+    client = ModbusTcpClient(server_ip, server_port)
+
+    # Connect to the server
     client.connect()
 
+    # Define the Modbus register address you want to read (e.g., register 1000)
+    register_address = 1000
+
     try:
+        # Read a single register (function code 3 - Read Holding Registers)
+        response = client.read_holding_registers(register_address, 1, unit=1)
+    
+        if not response.isError():
+            # Extract the value from the response
+            value = response.registers[0]
+            print(f'Read register {register_address}: {value}')
+        else:
+            print(f'Error reading register {register_address}: {response}')
+    except Exception as e:
+        print(f'Error: {e}')
+    finally:
+        # Close the connection
+        client.close()
+
+
+
+
+
+
+
+
+
+--------------
+    
+    # Create a Modbus TCP client
+    #client = ModbusTcpClient(modbus_host, port=modbus_port)
+
+    # Connect to the Modbus device
+    #client.connect()
+
+
+    
+    #try:
         # Read the registers as a block
-        res_pv = client.read_input_registers(ser_num, count=8, unit=unit_id)
+        #res_pv = client.read_input_registers(ser_num, count=8, unit=unit_id)
         #res_bil = client.read_input_registers(reg_bil, count=2, unit=unit_id)
         #res_wp = client.read_input_registers(reg_wp, count=2, unit=unit_id)
 
-        print(res_pv)
+        #print(res_pv)
         # Extract the values from the response
         #val_pv = res_pv.registers
         #val_bil = res_bil.registers
@@ -92,9 +129,9 @@ def main():
         #print(f"Value Home: {val_home}")
         #print(f"Value EIV: {val_eiv}")
         
-    finally:
+    #finally:
         # Close the Modbus connection
-        client.close()
+        #client.close()
 
     #write_vals(UUID["P_Home_Bilanz"], parsed_val_bil)
     #write_vals(UUID["P_Home_Verbrauch"], val_home)
