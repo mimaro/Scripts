683f452c160dd949898430495217539bcc6b2905
mimaro <46071857+mimaro@users.noreply.github.com>
2023-10-26 23:45:24 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -326,7 +326,7 @@ def main():
     write_vals(UUID["WW_Ein"], Ww_ein) 
     
     
-    logging.info("Ist-Wert WW-Temp ({}°C) < Einschalt-Wert WW-Temp ({}°C): WW_Freigabe {}".format(ww_temp,Ww_ein))
+    logging.info("Ist-Wert WW-Temp ({}°C) < Einschalt-Wert WW-Temp ({}°C)".format(ww_temp,Ww_ein))
     #logging.info("Ist-Wert WW-Temp ({}°C) >= Ausschalt-Wert WW-Temp ({}°C): WW_Sperrung {}".format(ww_temp,ww_aus,Ww_aus))
     logging.info("Aktuelle Uhrzeit ({}) in Zeitfenster ({} - {} Uhr): {}".format(now.time(),ww_start,ww_stop,ww_time))
    
