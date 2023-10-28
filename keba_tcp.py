3e7c571d92591dd61cb78eec3555a924ae95633e
mimaro <46071857+mimaro@users.noreply.github.com>
2023-10-28 13:26:31 +0200

    Update keba_tcp.py

--- keba_tcp.py
+++ keba_tcp.py
@@ -142,7 +142,7 @@ def main():
     res_bil = client_sel.read_input_registers(reg_bil, count=2, unit=1)
     val_bil = res_bil.registers
     byte_string_bil = struct.pack('>HH', val_bil[0], val_bil[1])     
-    parsed_val_bil = int((struct.unpack('>i', byte_string_bil)[0])/100)
+    parsed_val_bil = int((struct.unpack('>i', byte_string_bil)[0])/100)-500
 
     # Berechne Bilanz Wagenrain in A
     val_bil_i = ((parsed_val_bil-bil_offset) / (curr_v_val))*-1
