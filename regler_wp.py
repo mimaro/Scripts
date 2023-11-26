997740581f9518e755b03b91c8fb71df4eeae69c
mimaro <46071857+mimaro@users.noreply.github.com>
2023-11-26 21:29:02 +0100

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -125,7 +125,8 @@ def main():
     #######################################################################################################
     logging.info(f"---------- Prüfung Freigabe / Sperrung Heizgrenze ----------") 
     # Abfrage aktuelle Aussentemperatur
-    t_now = get_vals(UUID["T_outdoor"])["data"]["tuples"][0][1]
+    #t_now = get_vals(UUID["T_outdoor"])["data"]["tuples"][0][1]
+    t_now = (CLIENT.read_input_registers(REGISTER["AT"], count=1, unit=1).getRegister(0))/10
     
     # Abfragen 24h Aussentemperatur und ggf. Freigabe Heizgrenze
     t_roll_avg_24 = get_vals(UUID["T_outdoor"], duration="-1440min")["data"]["average"]
