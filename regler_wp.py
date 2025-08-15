7c6669ed2aaacebe0c8639797f65df7ba84407e9
mimaro <46071857+mimaro@users.noreply.github.com>
2025-08-16 00:17:10 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -416,11 +416,11 @@ def main():
     else:
         rt_ist_hk_2 == rt_ist_hk_2
 
-    if t_puffer_unten <= 18 or rt_ist_hk_2_puffer < 23.4:
+    if t_puffer_unten <= 18 or rt_ist_hk_2_puffer < 20:
         freigabe_kühlen = 0
         CLIENT.write_register(REGISTER["RT_SOLL_KK2"], 280)
 
-    elif t_puffer_unten > 18.5 and rt_ist_hk_2_puffer > 23.6:
+    elif t_puffer_unten > 18.5 and rt_ist_hk_2_puffer > 20:
         freigabe_kühlen = 1
         CLIENT.write_register(REGISTER["RT_SOLL_KK2"], 230)
 
