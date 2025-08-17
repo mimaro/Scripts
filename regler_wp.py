8e3ad488b5966aec6be5a812f4dfadeb771ff5b8
mimaro <46071857+mimaro@users.noreply.github.com>
2025-08-17 12:52:02 +0200

    Update regler_wp.py

--- regler_wp.py
+++ regler_wp.py
@@ -408,7 +408,7 @@ def main():
     s_freigabe_kühlen = get_vals(UUID["S_FREIGABE_KÜHLEN"], duration="-1min")["data"]["average"]
     rt_ist_hk_2 = (CLIENT.read_input_registers(REGISTER["RT_IST_OG"], count=1, unit=1).getRegister(0))/10
     rt_soll_hk_2 = (CLIENT.read_holding_registers(REGISTER["RT_SOLL_KK2"], count=1, unit= 1).getRegister(0))/10 
-    rt_ist_hk_2_puffer = get_vals(UUID["T_Raum_OG"], duration="-20min")["data"]["average"]
+    rt_ist_hk_2_puffer = get_vals(UUID["T_Raum_OG"], duration="-1440min")["data"]["average"]
     t_taupunkt = (CLIENT.read_input_registers(590, count=1, unit=1).getRegister(0))/10
     
     if rt_ist_hk_2 > 30:
@@ -416,11 +416,11 @@ def main():
     else:
         rt_ist_hk_2 == rt_ist_hk_2
 
-    if t_puffer_unten <= 18 or rt_ist_hk_2_puffer < 20:
+    if t_puffer_unten <= 18 or rt_ist_hk_2_puffer < 22:
         freigabe_kühlen = 0
         CLIENT.write_register(REGISTER["RT_SOLL_KK2"], 280)
 
-    elif t_puffer_unten > 18.5 and rt_ist_hk_2_puffer > 20:
+    elif t_puffer_unten > 18.5 and rt_ist_hk_2_puffer > 22:
         freigabe_kühlen = 1
         CLIENT.write_register(REGISTER["RT_SOLL_KK2"], 230)
 
