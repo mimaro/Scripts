f8cb7a53aed121a2dec29ffbe7f5f65e0b1647ee
mimaro <46071857+mimaro@users.noreply.github.com>
2025-10-15 23:23:02 +0200

    Update forecast_day.py

--- forecast_day.py
+++ forecast_day.py
@@ -118,7 +118,7 @@ def main():
     tz = pytz.timezone('Europe/Zurich')
     now = datetime.datetime.now(tz=tz)
 
-    test = get_vals(UUID["Freigabe_WP_Opt"], duration="now&to=900min")["data"]
+    test = get_vals(UUID["Freigabe_WP_Opt"], duration="now&to=+900min")["data"]
     tuples_test = test.get("tuples", [])
     print(tuples_test)
     
