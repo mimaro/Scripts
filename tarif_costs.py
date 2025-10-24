0803273a5438cc134aacce136f78ea95e969b0e4
mimaro <46071857+mimaro@users.noreply.github.com>
2025-10-24 14:17:50 +0200

    Update tarif_costs.py

--- tarif_costs.py
+++ tarif_costs.py
@@ -62,10 +62,10 @@ def get_data(uuid, duration="-15min"):
 def main():
 
     #Energie exkl PV letzte 15 Minuten Abfragen
-    brutto_energie = get_vals(UUID["Brutto_Energie"], duration="-15min&to=now")["data"]["consumption"]/60
+    brutto_energie = get_vals(UUID["Brutto_Energie"], duration="-15min")["data"]["consumption"]/60
     
     #Energie inkl PV letzte 15 Minuten Abfragen
-    netto_energie = get_vals(UUID["Netto_Energie"], duration="-15min&to=now")["data"]["consumption"]/60
+    netto_energie = get_vals(UUID["Netto_Energie"], duration="-15min")["data"]["consumption"]/60
   
     preis_stat = 27.129
     preis_dyn = get_vals(UUID["Preis_dyn"], duration="-15min&to=now")["data"]["average"]
